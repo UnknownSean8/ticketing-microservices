441fd2591ee28eda7bf371295405ed0bb85dad12
Sean Leong <seanleong888@gmail.com>
2023-04-18 01:37:15 +0800

    feat: added type check for user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<any> {
+    build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -10,7 +19,10 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 });
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
 export { User };
